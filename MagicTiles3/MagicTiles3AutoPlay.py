e3d3e3baafcda738602c6faae93563ddce72ac00
dude1180 <dudey307@gmail.com>
2026-05-29 15:34:18 +0700

    Enhance MagicTiles3AutoPlay with pause and color checks
    Added pause/resume functionality and improved color detection.

--- MagicTiles3/MagicTiles3AutoPlay.py
+++ MagicTiles3/MagicTiles3AutoPlay.py
@@ -3,18 +3,21 @@ import keyboard
 import time
 
 # UBAH VARIABLE MENYESUAIKAN DENGAN LAYAR ANDA
-y_trigger = xxx #kordinat y untuk mendeteksi tiles
+y_trigger = xxx  #kordinat y untuk mendeteksi tiles
 
 lanes = [xxx, xxx, xxx, xxx] #kordinat x untuk masing-masing lane (dari kiri ke kanan)
 keys  = ['w', 'a', 's', 'd']
 
 key_states = [False, False, False, False]
+is_paused = False 
 
-print("=== MAGIC TILES BOT By dude1180 ===")
-print("Tekan '-' untuk MULAI, tahan 'q' untuk BERHENTI.")
+print("=== MAGIC TILES BOT BY DUDE1180) ===")
+print("tekan '-' untuk MULAI pertama kali.")
+print("tekan 'p' kapan aja untuk PAUSE / RESUME.")
+print("tekan 'q' untuk BERHENTI total.")
 
 keyboard.wait('-')
-print("\nSistem Aktif! Memindai...")
+print("\nsistem aktif!")
 
 with mss.MSS() as sct:
     grab_box = {
@@ -25,6 +28,20 @@ with mss.MSS() as sct:
     }
 
     while not keyboard.is_pressed('q'):
+        if keyboard.is_pressed('p'):
+            is_paused = not is_paused
+            
+            if is_paused:
+                print("\n[PAUSED] Bot istirahat. Tekan 'p' lagi untuk lanjut.")
+                for i, k in enumerate(keys):
+                    if key_states[i]:
+                        keyboard.release(k)
+                        key_states[i] = False
+            else:
+                print("\n[RESUMED] Bot kembali jalan!")
+            time.sleep(0.3) 
+
+            continue
         img = sct.grab(grab_box)
 
         for i in range(4):
@@ -34,19 +51,19 @@ with mss.MSS() as sct:
             r, g, b = p[0], p[1], p[2]
 
             is_black = (r < 80 and g < 80 and b < 80)
-            
             is_purple = (r < 140 and g < 70 and b > 150)
+            is_cyan = (r < 60 and g > 150 and b > 200)
 
-            if is_black or is_purple:
+            if is_black or is_purple or is_cyan:
                 if not key_states[i]:
                     keyboard.press(keys[i])
                     key_states[i] = True
+                    time.sleep(0.005) 
             else:
                 if key_states[i]:
                     keyboard.release(keys[i])
                     key_states[i] = False
 
-# Failsafe
 for k in keys: 
     keyboard.release(k)
 
